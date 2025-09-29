bc8138593bf53b6a342797400280bb79d807ab09
John Burnett <github@johnburnett.com>
2025-09-29 16:50:42 -0700

    Include css files in export

--- main.py
+++ main.py
@@ -5,14 +5,21 @@
 #     "requests",
 # ]
 # ///
+# https://developers.beeper.com/desktop-api/
+# https://www.beeper.com/download/nightly/now
+#
 # - Old messages require scrolling back in the UI?
-# - Message.text is truncated ("insid... [+78 chars]")
+
+from dataclasses import dataclass
 import html
 import json
 import os
+import posixpath
 import re
 import shutil
 import sys
+import textwrap
+import typing
 
 import requests
 from rich import traceback
@@ -30,48 +37,39 @@ FILE_NAME_RESERVED_NAMES = {
 }
 FILE_NAME_RESERVED_CHARS_RE = re.compile(r'["*/:<>?\\|]')
 
-HTML_HEAD = '''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
-    <style>
-        section {
-            margin-bottom: 1rem;
-            width: 66%;
-            background-color: #3b3b3bff;
-            padding: 0.5rem;
-            border-radius: 5px;;
-        }
-        .you {
-            margin-left: 33%;
-            background-color: #195feeff;
-        }
-        span {
-            font-size: 90%;
-            display: flex;
-            justify-content: end;
-        }
-    </style>
-</head>
-<body>
-'''
+@dataclass
+class ExportContext:
+    fout: typing.TextIO
+    self_id: str
+    attachment_dir_path: str
+    css_url_sub_dir: str
 
-HTML_TAIL = '''
-</body>
-</html>
-'''
+def getHtmlHead(exportCssSubDir):
+    head = f'''<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Chat</title>
+        <link rel="stylesheet" href="{exportCssSubDir}/water.css">
+        <link rel="stylesheet" href="{exportCssSubDir}/extra.css">
+    </head>
+    <body>
+    '''
+    return textwrap.dedent(head)
 
-def sanitize_filename(name):
-    if name.casefold() in FILE_NAME_RESERVED_NAMES:
-        name = name + '_'
-    name = FILE_NAME_RESERVED_CHARS_RE.sub('', name)
-    name = name.strip(' \t\n.')
-    return name if name else '_'
+def getHtmlTail():
+    return '''
+    </body>
+    </html>
+    '''
 
+def sanitize_file_name(file_name):
+    if file_name.casefold() in FILE_NAME_RESERVED_NAMES:
+        file_name = file_name + '_'
+    file_name = FILE_NAME_RESERVED_CHARS_RE.sub('', file_name)
+    file_name = file_name.strip(' \t\n.')
+    return file_name if file_name else '_'
 
 def chat_details_to_html(fout, chat_details):
     fout.write(f'<dl>\n')
@@ -96,33 +94,33 @@ def download_attachment(attachment_dir_path, chat_details, item, att):
         resp.raise_for_status()
         data = resp.json()
         source_url = data['srcURL']
-
     assert source_url.startswith('file:///')
-    source_path = source_url[len('file:///'):]
+    source_file_path = source_url[len('file:///'):]
     if os.path.sep != '/':
-        source_path = source_path.replace('/', os.path.sep)
+        source_file_path = source_file_path.replace('/', os.path.sep)
     os.makedirs(attachment_dir_path, exist_ok=True)
-    target_path = os.path.join(attachment_dir_path, os.path.basename(source_path))
-    shutil.copy2(source_path, target_path)
+    target_file_name = sanitize_file_name(os.path.basename(source_file_path))
+    target_file_path = os.path.join(attachment_dir_path, target_file_name)
+    if not os.path.exists(target_file_path):
+        shutil.copy2(source_file_path, target_file_path)
     if os.path.sep != '/':
-        target_path = target_path.replace(os.path.sep, '/')
-    target_path = 'file:///' + target_path
-    return target_path
+        target_file_path = target_file_path.replace(os.path.sep, '/')
+    target_file_path = 'file:///' + target_file_path
+    return target_file_path
 
 
-def message_to_html(fout, chat_details, msg, self_id, attachment_dir_path):
+def message_to_html(ctx: ExportContext, chat_details, msg):
     # from pprint import pformat
-    # fout.write(f'<section><pre>{pformat(msg)}</pre></section>\n')
+    # ctx.fout.write(f'<section><pre>{pformat(msg)}</pre></section>\n')
     # return
 
     sec_class = 'you' if self_id == msg.get('senderID') else 'them'
     fout.write(f'<section class="{sec_class}">{msg["senderName"]}:<br>\n')
     if 'text' in msg:
         msg_text = html.escape(msg['text'], quote=False)
-        fout.write(msg_text)
+        ctx.fout.write(msg_text)
     for att in msg.get('attachments', []):
-        # fout.write(f'<div>Attachment: type="{att["type"]}", fileSize={att["fileSize"]}, fileName="{att["fileName"]}"</div>\n')
-        att_url = download_attachment(attachment_dir_path, chat_details, msg, att)
+        att_url = download_attachment(ctx.attachment_dir_path, chat_details, msg, att)
 
         dim_attr = ''
         size = att.get('size', {})
@@ -131,63 +129,81 @@ def message_to_html(fout, chat_details, msg, self_id, attachment_dir_path):
 
         att_type = att.get('type')
         if att_type == 'img':
-            fout.write(f'<img loading="lazy"{dim_attr} src="{att_url}"/>\n')
+            ctx.fout.write(f'<img loading="lazy"{dim_attr} src="{att_url}"/>\n')
         elif att_type == 'video':
-            fout.write(f'<video controls loop playsinline{dim_attr} src="{att_url}"/>\n')
+            ctx.fout.write(f'<video controls loop playsinline{dim_attr} src="{att_url}"/>\n')
         elif att_type == 'audio':
-            fout.write(f'<audio controls src="{att_url}"/>\n')
-    fout.write(f'<span>{msg["timestamp"]}</span></section>\n')
+            ctx.fout.write(f'<audio controls src="{att_url}"/>\n')
+    ctx.fout.write(f'<span>{msg["timestamp"]}</span></section>\n')
 
 
-def messages_to_html(fout, chat_details, items, attachment_dir_path):
-    if not items:
+def messages_to_html(ctx: ExportContext, chat_details, msgs):
+    if not msgs:
         return
 
     chat_id = chat_details['id']
-    for item in items:
-        assert item['chatID'] == chat_id
+    for msg in msgs:
+        assert msg['chatID'] == chat_id
 
-    self_id = None
-    for part in chat_details.get('participants', {}).get('items', []):
-        if part.get('isSelf', False):
-            self_id = part.get('id')
+    ctx.fout.write(getHtmlHead(posixpath.join('../..', ctx.css_url_sub_dir)))
 
-    fout.write(HTML_HEAD)
+    ctx.fout.write('<header>\n')
+    chat_details_to_html(ctx.fout, chat_details)
+    ctx.fout.write('</header>\n')
 
-    fout.write('<header>\n')
-    chat_details_to_html(fout, chat_details)
-    fout.write('</header>\n')
+    ctx.fout.write('<main>\n')
+    for msg in msgs:
+        message_to_html(ctx, chat_details, msg)
+    ctx.fout.write('</main>\n')
 
-    fout.write('<main>\n')
-    for item in items:
-        message_to_html(fout, chat_details, item, self_id, attachment_dir_path)
-    fout.write('</main>\n')
+    ctx.fout.write(getHtmlTail())
 
-    fout.write(HTML_TAIL)
+
+def copy_css_files(output_root_dir_path, data_dir_name):
+    source_dir_path = os.path.join(os.path.dirname(__name__), 'css')
+    assert os.path.isdir(source_dir_path)
+    css_url_sub_dir = posixpath.join(data_dir_name, 'css') # relative to root output dir
+    target_dir_path = os.path.join(output_root_dir_path, css_url_sub_dir)
+    os.makedirs(target_dir_path, exist_ok=True)
+    for file_name in ('water.css', 'extra.css'):
+        source_file_path = os.path.join(source_dir_path, file_name)
+        target_file_path = os.path.join(target_dir_path, file_name)
+        shutil.copy(source_file_path, target_file_path)
+    return css_url_sub_dir
 
 
 def dump_data(data, output_root_dir):
+    msgs = data['items']
     chat_to_messages = {}
-    for item in data['items']:
-        chat_to_messages.setdefault(item['chatID'], []).append(item)
+    for msg in msgs:
+        chat_to_messages.setdefault(msg['chatID'], []).append(msg)
+
+    data_dir_name = '_data'
+    css_url_sub_dir = copy_css_files(output_root_dir, data_dir_name)
 
     chat_id_to_chat_details = data['chats']
 
     chat_index = 0
-    for chat_id, items in chat_to_messages.items():
+    for chat_id, msgs in chat_to_messages.items():
         chat_details = chat_id_to_chat_details[chat_id]
-        items.sort(key=lambda it: it['sortKey'])
+        self_id = None
+        for part in chat_details.get('participants', {}).get('items', []):
+            if part.get('isSelf', False):
+                self_id = part.get('id')
+        assert self_id
+        msgs.sort(key=lambda it: it['sortKey'])
 
-        output_dir_name = sanitize_filename(chat_details['title'])
-        output_dir_path = os.path.join(output_root_dir, chat_details['network'].lower(), output_dir_name)
+        network_dir_name = sanitize_file_name(chat_details['network'].lower())
+        output_dir_name = sanitize_file_name(chat_details['title'])
+        output_dir_path = os.path.join(output_root_dir, network_dir_name, output_dir_name)
         os.makedirs(output_dir_path, exist_ok=True)
 
         attachment_dir_path = os.path.join(output_dir_path, 'attachments')
 
         output_file_path = os.path.join(output_dir_path, 'chat.html')
         with open(output_file_path, 'w', encoding='utf-8') as fp:
-
-            messages_to_html(fp, chat_details, items, attachment_dir_path)
+            context = ExportContext(fp, self_id, attachment_dir_path, css_url_sub_dir)
+            messages_to_html(context, chat_details, msgs)
         chat_index += 1
 
 
@@ -198,10 +214,12 @@ def get_all_messages():
         'chats': {},
     }
     while True:
-        params = {"limit": 20}  # 20 is the cap per page
+        params = {'limit': 20}  # 20 is the cap per page
+        params['excludeLowPriority'] = False
+        params['includeMuted'] = True
         if cursor:
-            params["cursor"] = cursor
-            params["direction"] = "before"
+            params['cursor'] = cursor
+            params['direction'] = 'before'
 
         resp = requests.get(f'{BEEPER_HOST_URL}/v0/search-messages', headers=REQUEST_HEADERS, params=params)
         resp.raise_for_status()
@@ -209,10 +227,9 @@ def get_all_messages():
 
         all_data['chats'].update(data.get('chats', {}))
         all_data['items'].extend(data.get('items', []))
-        # print(f"Fetched {len(items)} messages (total {len(all_messages)})")
-        if not data.get("hasMore") or not data.get("oldestCursor"):
+        if not data.get('hasMore') or not data.get('oldestCursor'):
             break
-        cursor = data["oldestCursor"]
+        cursor = data['oldestCursor']
     return all_data
 
 
