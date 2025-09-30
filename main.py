3d190f0288762768eb9a931b0585df80cd201d9c
John Burnett <github@johnburnett.com>
2025-09-30 14:25:04 -0700

    Re-org export layout, make chat html nicer

--- main.py
+++ main.py
@@ -11,6 +11,7 @@
 # - Old messages require scrolling back in the UI?
 
 from dataclasses import dataclass
+from datetime import datetime
 import html
 import os
 import posixpath
@@ -20,6 +21,7 @@ import sys
 import textwrap
 import typing
 
+import bleach
 import requests
 from rich import traceback
 
@@ -47,21 +49,22 @@ FILE_NAME_RESERVED_CHARS_RE = re.compile(r'["*/:<>?\\|]')
 
 @dataclass
 class ExportContext:
+    output_file_path: str
     fout: typing.TextIO
-    self_id: str
     attachment_dir_path: str
     css_url_sub_dir: str
+    self_id: str
 
 
-def getHtmlHead(exportCssSubDir):
+def getHtmlHead(title, export_css_sub_dir):
     head = f'''<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Chat</title>
-        <link rel="stylesheet" href="{exportCssSubDir}/water.css">
-        <link rel="stylesheet" href="{exportCssSubDir}/extra.css">
+        <title>Chat: {title}</title>
+        <link rel="stylesheet" href="{export_css_sub_dir}/water.css">
+        <link rel="stylesheet" href="{export_css_sub_dir}/extra.css">
     </head>
     <body>
     '''
@@ -84,22 +87,28 @@ def sanitize_file_name(file_name):
 
 
 def chat_details_to_html(fout, chat_details):
-    fout.write("<dl>\n")
-    fout.write(f"<dt>title</dt><dd>{chat_details['title']}</dd>\n")
-    fout.write("</dl>\n")
-    fout.write(f"<dt>accountID</dt><dd>{chat_details.get('accountID')}</dd>\n")
-    fout.write(f"<dt>network</dt><dd>{chat_details.get('network')}</dd>\n")
-    fout.write(f"<dt>chatID</dt><dd>{chat_details.get('id')}</dd>\n")
-    fout.write("<dt>participants</dt><dd>\n")
-    fout.write("<ul>\n")
-    for part in chat_details.get("participants", {}).get("items", []):
-        fout.write(f"  <li>{part.get('fullName', part.get('id', 'Unknown'))}</li>\n")
-    fout.write("</ul>\n")
-    fout.write("</dd>\n")
-    fout.write("</dl>\n")
+    fout.write('<section class="chat-header">\n')
+    fout.write(f"<h1>{chat_details['title']}</h1>\n")
+    fout.write('<section class="chat-details">(')
+    fout.write(
+        f'<span class="chat-details-label">Network: </span><span>{chat_details.get("network")}</span>, '
+    )
+    fout.write(
+        f'<span class="chat-details-label">Account ID: </span><span>{chat_details.get("accountID")}</span>, '
+    )
+    fout.write(
+        f'<span class="chat-details-label">Chat ID: </span><span>{chat_details.get("id")}</span>'
+    )
+    fout.write(")</section>\n")
+    fout.write("<h2>Participants</h2>\n")
+    parts = chat_details.get("participants", {}).get("items", [])
+    names = [part.get("fullName", part.get("id")) for part in parts]
+    for name in sorted(names):
+        fout.write(f"<div>{name}</div>\n")
+    fout.write("</section>")
 
 
-def download_attachment(attachment_dir_path, chat_details, item, att):
+def download_attachment(attachment_dir_path, msg, att):
     source_url = att["srcURL"]
     if source_url.startswith("mxc://"):
         resp = requests.post(
@@ -115,13 +124,13 @@ def download_attachment(attachment_dir_path, chat_details, item, att):
     if os.path.sep != "/":
         source_file_path = source_file_path.replace("/", os.path.sep)
     os.makedirs(attachment_dir_path, exist_ok=True)
-    target_file_name = sanitize_file_name(os.path.basename(source_file_path))
+    target_file_name, target_file_ext = os.path.splitext(att["fileName"])
+    target_file_name = (
+        sanitize_file_name(f"{msg['timestamp']}_{target_file_name}") + target_file_ext
+    )
     target_file_path = os.path.join(attachment_dir_path, target_file_name)
     if not os.path.exists(target_file_path):
         shutil.copy2(source_file_path, target_file_path)
-    if os.path.sep != "/":
-        target_file_path = target_file_path.replace(os.path.sep, "/")
-    target_file_path = "file:///" + target_file_path
     return target_file_path
 
 
@@ -130,14 +139,29 @@ def message_to_html(ctx: ExportContext, chat_details, msg):
     # ctx.fout.write(f'<section><pre>{pformat(msg)}</pre></section>\n')
     # return
 
-    sec_class = "you" if ctx.self_id == msg.get("senderID") else "them"
-    ctx.fout.write(f'<section class="{sec_class}">{msg["senderName"]}:<br>\n')
+    sec_class = "msg-self" if ctx.self_id == msg.get("senderID") else "msg-them"
+    message_id = msg["messageID"]
+    ts_utc = datetime.fromisoformat(msg["timestamp"])
+    ts_local = ts_utc.astimezone()
+    ts_utc_str = ts_utc.strftime("%Y-%m-%d %H:%M:%S %Z")
+    ts_local_str = ts_local.strftime("%Y-%m-%d %H:%M:%S %Z")
+    ctx.fout.write(
+        f'<section class="msg {sec_class}"><div id="{message_id}" class="msg-header"><span class="msg-contact-name">{msg["senderName"]}</span><span class="msg-datetime" title="{ts_utc_str}">{ts_local_str}</span><a class="permalink" title="Message {message_id}" href="#{message_id}">&#x1F517;&#xFE0E;</a></div>\n'
+    )
+
     if "text" in msg:
         msg_text = html.escape(msg["text"], quote=False)
         msg_text = msg_text.replace("\n", "<br>\n")
+        msg_text = bleach.linkify(msg_text)
         ctx.fout.write(msg_text)
+
     for att in msg.get("attachments", []):
-        att_url = download_attachment(ctx.attachment_dir_path, chat_details, msg, att)
+        att_file_path = download_attachment(ctx.attachment_dir_path, msg, att)
+        att_url = os.path.relpath(
+            att_file_path, start=os.path.dirname(ctx.output_file_path)
+        )
+        if os.path.sep != "/":
+            att_url = att_url.replace("\\", "/")
 
         dim_attr = ""
         size = att.get("size", {})
@@ -146,14 +170,17 @@ def message_to_html(ctx: ExportContext, chat_details, msg):
 
         att_type = att.get("type")
         if att_type == "img":
-            ctx.fout.write(f'<img loading="lazy"{dim_attr} src="{att_url}"/>\n')
+            ctx.fout.write(
+                f'<a href="{att_url}"><img loading="lazy"{dim_attr} src="{att_url}"/></a>\n'
+            )
         elif att_type == "video":
             ctx.fout.write(
                 f'<video controls loop playsinline{dim_attr} src="{att_url}"/>\n'
             )
         elif att_type == "audio":
             ctx.fout.write(f'<audio controls src="{att_url}"/>\n')
-    ctx.fout.write(f"<span>{msg['timestamp']}</span></section>\n")
+
+    ctx.fout.write("</section>\n")
 
 
 def messages_to_html(ctx: ExportContext, chat_details, msgs):
@@ -164,7 +191,9 @@ def messages_to_html(ctx: ExportContext, chat_details, msgs):
     for msg in msgs:
         assert msg["chatID"] == chat_id
 
-    ctx.fout.write(getHtmlHead(posixpath.join("../..", ctx.css_url_sub_dir)))
+    ctx.fout.write(
+        getHtmlHead(chat_details["title"], posixpath.join("../..", ctx.css_url_sub_dir))
+    )
 
     ctx.fout.write("<header>\n")
     chat_details_to_html(ctx.fout, chat_details)
@@ -181,9 +210,7 @@ def messages_to_html(ctx: ExportContext, chat_details, msgs):
 def copy_css_files(output_root_dir_path, data_dir_name):
     source_dir_path = os.path.join(os.path.dirname(__name__), "css")
     assert os.path.isdir(source_dir_path)
-    css_url_sub_dir = posixpath.join(
-        data_dir_name, "css"
-    )  # relative to root output dir
+    css_url_sub_dir = posixpath.join(data_dir_name, "beepex")
     target_dir_path = os.path.join(output_root_dir_path, css_url_sub_dir)
     os.makedirs(target_dir_path, exist_ok=True)
     for file_name in ("water.css", "extra.css"):
@@ -199,8 +226,7 @@ def dump_data(data, output_root_dir):
     for msg in msgs:
         chat_to_messages.setdefault(msg["chatID"], []).append(msg)
 
-    data_dir_name = "_data"
-    css_url_sub_dir = copy_css_files(output_root_dir, data_dir_name)
+    css_url_sub_dir = copy_css_files(output_root_dir, "media")
 
     chat_id_to_chat_details = data["chats"]
 
@@ -215,17 +241,18 @@ def dump_data(data, output_root_dir):
         msgs.sort(key=lambda it: it["sortKey"])
 
         network_dir_name = sanitize_file_name(chat_details["network"].lower())
-        output_dir_name = sanitize_file_name(chat_details["title"])
-        output_dir_path = os.path.join(
-            output_root_dir, network_dir_name, output_dir_name
-        )
+        chat_title = sanitize_file_name(chat_details["title"])
+        output_dir_path = os.path.join(output_root_dir, "chats", network_dir_name)
         os.makedirs(output_dir_path, exist_ok=True)
 
-        attachment_dir_path = os.path.join(output_dir_path, "attachments")
-
-        output_file_path = os.path.join(output_dir_path, "chat.html")
+        output_file_path = os.path.join(output_dir_path, chat_title + ".html")
+        attachment_dir_path = os.path.join(
+            output_root_dir, "media", network_dir_name, chat_title
+        )
         with open(output_file_path, "w", encoding="utf-8") as fp:
-            context = ExportContext(fp, self_id, attachment_dir_path, css_url_sub_dir)
+            context = ExportContext(
+                output_file_path, fp, attachment_dir_path, css_url_sub_dir, self_id
+            )
             messages_to_html(context, chat_details, msgs)
         chat_index += 1
 
